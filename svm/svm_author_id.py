1a017f8dcccdc90163c2172904cf3af44b6477e0
vagrant <vagrant@udacity.puphpet>
2019-11-13 17:25:54 +0000

    Lesson 3 Added SVM training, predicting and calculating accuracy

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,5 +26,48 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn.svm import SVC
+# Linear
+# clf = SVC(kernel="linear")
+
+# rbf, default gamma
+# clf = SVC(kernel="rbf", gamma="auto")
+# rbf, using gamma=scale increases accuracy
+# clf = SVC(kernel="rbf", gamma="scale")
 
 
+# Reduce the training set to 1% of original data
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+#CList = [10, 100, 1000, 10000]
+# highest C is most accurate
+CList = [10000]
+
+for CVal in CList:
+    clf = SVC(kernel="rbf", gamma="auto", C=CVal)
+    print "####   Test for C=", CVal, "  ####"
+    print "Start training ..."
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "Training time:", round(time() - t0, 3), "s"
+
+    print "Start predicting ..."
+    t0 = time()
+    pred = clf.predict(features_test)
+    print "Predicting time:", round(time() - t0,3), "s"
+
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(labels_test, pred)
+    print "Accuracy: ", accuracy
+
+    answers = [10, 26, 50]
+    people = ["Sara", "Chris"]
+    for answer in answers:
+        print "Answer for email #", answer, "is: ", pred[answer], " => ", people[pred[answer]]
+
+    print "Total emails predicted to be from Sara (0): ", (pred == 0).sum()
+    print "Total emails predicted to be from Chris (1): ", (pred == 1).sum()
+
+
+    print "##################################"
